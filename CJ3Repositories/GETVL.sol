a51485e4423a9d83f12f5cc0dcf98494a8fed084
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 13:14:37 -0600

    Update GETVL.sol

--- CJ3Repositories/GETVL.sol
+++ CJ3Repositories/GETVL.sol
@@ -1,1 +1,18 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "../core/VaultBase.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract GETVL is VaultBase {
+    IERC20 public immutable asset;
+
+    constructor(IERC20 _asset) {
+        asset = _asset;
+    }
+
+    function deposit(uint256 amount) external {
+        asset.transferFrom(msg.sender, address(this), amount);
+    }
+}
