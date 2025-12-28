c30e632a62100d794388d27e335c200943334ca2
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 13:35:48 -0600

    Update InstilledInteroperability.sol

--- CJ3Repositories/InstilledInteroperability.sol
+++ CJ3Repositories/InstilledInteroperability.sol
@@ -216,3 +216,10 @@ else if polyVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymb
 
 
 return default InstilledInteroperability;
+
+_____________________
+
+contingency: 80% revenue allocated to $CJ3Reserve as $USDC, 20% to sdk/api user;
+subscription fee: $25/ a month OR +5% revenue allocated to the owner address;
+amount $CJ3 to stake to deploy to mainnet: $100/ per contract using in sdk/api (allocate to $CJ3Reserves as $XLM)
+
