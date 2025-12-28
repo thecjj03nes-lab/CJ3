47397ffe6dff63282ba748f3bc894f019f7a1c1a
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 12:40:29 -0600

    Update devHelper.sol

--- devHelper.sol
+++ devHelper.sol
@@ -1,1 +1,5 @@
+( an aggregation of chatgpt, onspace ai, perplexity ai, genspark ai, base44, github copilot, cursor, replit ghostwriter, amazon ghostwhisperer, gemini, windsurf, qodo, tabnine, lovable, aider, mintlify, and graphite ai) 
 
+- choose their type of business / business they are creating (bank, exchange, utility company, telecommunications, government, prediction market, social media, other)
+- choose the different types of products they want implemented in their custom sdk/api from $CJ3 technology & repositories
+- devHelper helps them build their feature, webisite, app, and etc based on subscription + sdk/api contingency)
