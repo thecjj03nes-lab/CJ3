3fef55c627eeaf4461a94e26b239daee1a6fbfbe
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 13:31:35 -0600

    Update CloudMiningNFT.sol

--- CJ3Repositories/CloudMiningNFT.sol
+++ CJ3Repositories/CloudMiningNFT.sol
@@ -20,6 +20,6 @@ contract CloudMiningNFT is ERC721 {
 
 _____________________
 
-contingency: 30% allocated to $CJ3Reserve as $USDC, 70% to sdk/api user;
-subscription fee: $25/ a month OR +5% allocated to the owner address;
+contingency: 30% revenue allocated to $CJ3Reserve as $USDC, 70% to sdk/api user;
+subscription fee: $25/ a month OR +5% revenue allocated to the owner address;
 amount $CJ3 to stake to deploy to mainnet: $100/ per contract using in sdk/api (allocate to $CJ3Reserves as $XLM)
