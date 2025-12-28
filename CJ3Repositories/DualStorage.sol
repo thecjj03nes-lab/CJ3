5f11d577544e8f471d3fdd5d7d331d56365fa3cb
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 13:32:28 -0600

    Update DualStorage.sol

--- CJ3Repositories/DualStorage.sol
+++ CJ3Repositories/DualStorage.sol
@@ -72,6 +72,7 @@ so that it's simple, no revisions needed, and will process the same, but transpa
 
 _____________________
 
-contingency: 30% allocated to $CJ3Reserve as $USDC, 70% to sdk/api user;
-subscription fee: $25/ a month OR +5% allocated to the owner address;
+contingency: 30% revenue allocated to $CJ3Reserve as $USDC, 70% to sdk/api user;
+subscription fee: $25/ a month OR +5% revenue allocated to the owner address;
 amount $CJ3 to stake to deploy to mainnet: $100/ per contract using in sdk/api (allocate to $CJ3Reserves as $XLM)
+
