df1e40ddb89e81579bfebcc311ac18f896851e42
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 13:32:14 -0600

    Update DeviceManager.sol

--- CJ3Repositories/DeviceManager.sol
+++ CJ3Repositories/DeviceManager.sol
@@ -8,3 +8,8 @@ contract DeviceManager {
         deviceContribution[device] += value;
     }
 }
+_____________________
+
+contingency: 30% revenue allocated to $CJ3Reserve as $USDC, 70% to sdk/api user;
+subscription fee: $25/ a month OR +5% revenue allocated to the owner address;
+amount $CJ3 to stake to deploy to mainnet: $100/ per contract using in sdk/api (allocate to $CJ3Reserves as $XLM)
