994bae3d2e91359b69fc687e2f135d9cc1ed3f0e
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 12:51:25 -0600

    Create InsuranceAggregator.sol

---
+++ CJ3Repositories/InsuranceAggregator.sol
@@ -0,0 +1,56 @@
+pragma solidity ^0.8;
+
+// import kyc/aml info too;
+// import Pi {USD Denomination];
+
+
+contract InsuranceProviders{
+
+// enter age, sex, and citizenship info
+function vagueWho ={
+age = $age;
+gender = male || female;
+citizenship = $citizenship;
+zip code = $zipCode;
+employment status = working || not working;
+return vagueWho[information];}
+
+// choose type of insurance
+type of insurance needed = home || auto || life || health || motorcycle || bundle ;
+
+// find the best rates from an aggregation of providers
+function insuranceAggregation = {
+     (require == vagueWho[information]) ;
+[1. https://www.progressive.com/ ],
+[2. https://www.geico.com/],
+[3. https://www.statefarm.com/],
+[4. https://www.allstate.com/],
+[5. https://www.libertymutual.com/],
+[6. https://www.nationwide.com/] ;
+
+if geico price < progressive price return geico;
+if statefarm price > allstate price return allstate;
+if allstate price > progressive price return progressive;
+
+}
+
+
+// users pay in Pi => insurance agency accepts USD
+function payPiToPayUSD ={
+     (require == msg.sender[balance] );
+     (require == Pi[USD Denomination]);
+
+
+if Pi price = $50;
+if USD price = $1;
+if insurance[bill] = $100;
+then get Pi[USD Denomination];
+Pi[USD Denomination] is insurance[bill] % Pi price = amountOf Pi;
+2 = $100 % $50;
+100 PiInUSD = 2 Pi;
+
+return msg.sender[balance] - amountOf Pi = msg.sender[newBalance];
+then return function sell, amountOf Pi;
+await function sell, amountOf Pi => amountOf USD;
+then return amountOf USD + insuranceProvider[balance] = insuranceProvider[newBalance]; }
+
