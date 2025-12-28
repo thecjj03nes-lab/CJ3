ee98a0e5499fc6aa17e14172c2db42357d7401e5
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 13:35:30 -0600

    Update HomeTeamPicks.sol

--- CJ3Repositories/HomeTeamPicks.sol
+++ CJ3Repositories/HomeTeamPicks.sol
@@ -14,3 +14,10 @@ contract HomeTeamPicks {
         picks.push(Pick(msg.sender, prediction, msg.value));
     }
 }
+
+
+_____________________
+
+contingency: 80% revenue allocated to $CJ3Reserve as $USDC, 20% to sdk/api user;
+subscription fee: $25/ a month OR +5% revenue allocated to the owner address;
+amount $CJ3 to stake to deploy to mainnet: $100/ per contract using in sdk/api (allocate to $CJ3Reserves as $XLM)
