eda6ff3893537d40d33daf2c1cea34add97dd6d3
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 12:48:55 -0600

    Create DualStorage.sol

---
+++ CJ3Repositories/DualStorage.sol
@@ -0,0 +1,16 @@
+1) Dual-Storage
+
+Start backing $BTC with gold, stocks, ETFs, and yield tokens
+
+($BTC, $GOLD, $USDDenomination),
+($BTC, $yXLM, $USDDenomination),
+($BTC, $yETG, $USDDenomination),
+($BTC, $yBTC, $USDDenomination),
+($BTC, $yUSDC, $USDDenomination),
+($BTC, $NVDY, $USDDenomination),
+($BTC, $MSTY, $USDDenomination),
+
+and the same with $MSTR:
+
+($MSTR, $yBTC, $USDDenomination),
+($MSTR, $WMT, $USDDenomination),
