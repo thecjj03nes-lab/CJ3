ce7ec597b3f4e1dbaf1ae857dec15c2849d7b400
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 13:38:17 -0600

    Update quantumenergy.sol

--- CJ3Repositories/quantumenergy.sol
+++ CJ3Repositories/quantumenergy.sol
@@ -45,3 +45,8 @@ ZeropointShield
 
 qAI
 - quantum ai
+_____________________
+
+contingency: 80% revenue allocated to $CJ3Reserve as $USDC, 20% to sdk/api user;
+subscription fee: $25/ a month OR +5% revenue allocated to the owner address;
+amount $CJ3 to stake to deploy to mainnet: $100/ per contract using in sdk/api (allocate to $CJ3Reserves as $XLM)
