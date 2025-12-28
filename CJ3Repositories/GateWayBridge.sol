40697d43b7ed793d7e6a6009bd5b819392edd6dc
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 12:55:50 -0600

    Create GateWayBridge.sol

---
+++ CJ3Repositories/GateWayBridge.sol
@@ -0,0 +1,140 @@
+pragma solidity ^0.8;
+
+// physical digitized doorway
+// locationA => locationB
+// $25 one-way, $40 full-round 
+// 75% to coreteam, 25% to cj03nes
+import Zeropoint from "./Zeropoint.sol";
+import { connectDevice , deviceConnected, deviceInformation, accountBalances } from "./deviceConnect.sol" ; 
+owner.address = cj03nes;
+
+
+
+// chain to chain
+mapping(chainA) => mapping(chainB)  => crossChain;
+
+// chain to device
+mapping(chain) => mapping(device => mapping(deviceInformation) ) => deviceConnected;
+
+//on-chain Zeropoint to on-chaindevice
+mapping(chain => mapping(uint256 Zeropoint) => mapping(chain => mapping(device => mapping(deviceInformation) ) ) => zeropointConsumedToDevice;
+
+//on-chain ZeropointWifi to on-chain device
+mapping(chain => mapping(uint256 ZeropointWifi) => mapping(chain => mapping(device => mapping(deviceInformation) ) ) => zeropointwifiConsumedToDevice;
+
+// on-chain zeropoint and on-chain zeropointwifi to on-chain device
+mapping(chain => mapping(uint256 Zeropoint) ) && mapping(chain => mapping(uint256 ZeropointWifi) ) => mapping(chain => mapping(device => mapping(deviceInformation) ) ) zeropointAndZeropointWifiConsumedToDevice;
+
+// on-chain zeropoint and zeropointwifi to device location to on-chain zeropoint and zeropointwifi to device location
+mapping(chain => mapping(uint256 Zeropoint) && mapping(chain => mapping(uint256 ZeropointWifi) => mapping(device => mapping(deviceInformation => mapping ( deviceLocation ))))) => mapping(chain => mapping(uint256 Zeropoint) && mapping(chain => mapping(uint256 ZeropointWifi) => mapping(device => mapping(deviceInformation => mapping ( deviceLocation ))))) => deviceLocationAToDeviceLocationB;
+
+
+
+contract gateWayBridge ={
+
+// connect physical doorways
+function connectGateWay ={
+     (require == connectDevice);
+const connectDevice returns deviceConnected;
+
+deviceConnected = gateWayConnected;  }
+
+
+// create a bridge between digital doorwayA to digital doorwayB
+function hereToThere = {
+     (require == deviceConnected && gateWayConnected);
+     (require == zeropointAndZeropointWifiConsumedToDevice);
+
+// get the location of digital doorway A
+const chooseLocationA ={
+     (require == deviceConnected && gateWayConnected);
+     (require == zeropointAndZeropointWifiConsumedToDevice);
+     (require == gateWayConnected(deviceInformation[ipAdress && PhoneWifiMACAddress]) );
+}
+// get the location of digital doorway B
+const chooseLocationB ={
+     (require == deviceConnected && gateWayConnected);
+     (require == zeropointAndZeropointWifiConsumedToDevice); 
+     (require == gateWayConnected(deviceInformation[ipAddress && PhoneWifiMACAddress]) );
+
+
+return deviceLocationAToDeviceLocationB;  }     }
+
+
+
+
+function buyTicket ={
+     (require == verifiedChains);
+     (require == verifiedTokenAssets);
+     (require == msg.sender(balance);
+
+if msg.sender(balance) < ticketPrice then return error("Insufficient Funds"),
+else if msg.sender(balance) >= ticketPrice then return ticketPriceBought;
+msg.sender(balance) - ticketPriceBought = msg.sender(newBalance);
+owner.address(balance) + ticketPriceBought = owner.address(newBalance);
+
+
+
+
+newYork ={
+ticketPrice =[
+newYorkToLondon = ( $25 oneWay || $40 full-round ),
+newYorkToCalifornia = ( $25 oneWay || $40 full-round ),
+newYorkToHawaii = ( $25 oneWay || $40 full-round ),
+newYorkToDallas = ( $25 oneWay || $40 full-round ),
+newYorkToNewOrleans = ( $25 oneWay || $40 full-round ),
+newYorkToAtlanta = ( $25 oneWay || $40 full-round ),
+newYorkToFlorida = ( $25 oneWay || $40 full-round );   ]}
+
+
+dallas ={
+ticketPrice =[
+dallasToLondon = ( $25 oneWay || $40 full-round ),
+dallasToCalifornia = ( $25 oneWay || $40 full-round ),
+dallasToHawaii = ( $25 oneWay || $40 full-round ),
+dallasToNewYork = ( $25 oneWay || $40 full-round ),
+dallasToNewOrleans = ( $25 oneWay || $40 full-round ),
+dallasToAtlanta = ( $25 oneWay || $40 full-round ),
+dallasToFlorida = ( $25 oneWay || $40 full-round );   ]}
+
+
+newOrleans ={
+ticketPrice =[
+newOrleansToLondon = ( $25 oneWay || $40 full-round ),
+newOrleansToCalifornia = ( $25 oneWay || $40 full-round ),
+newOrleansToHawaii = ( $25 oneWay || $40 full-round ),
+newOrleansToNewYork = ( $25 oneWay || $40 full-round ),
+newOrleansToDallas = ( $25 oneWay || $40 full-round ),
+newOrleansToAtlanta = ( $25 oneWay || $40 full-round ),
+newOrleansToFlorida = ( $25 oneWay || $40 full-round );   ]}
+
+
+
+atlanta ={
+ticketPrice =[
+atlantaToLondon = ( $25 oneWay || $40 full-round ),
+atlantaToCalifornia = ( $25 oneWay || $40 full-round ),
+atlantaToHawaii = ( $25 oneWay || $40 full-round ),
+atlantaToNewYork = ( $25 oneWay || $40 full-round ),
+atlantaToDallas = ( $25 oneWay || $40 full-round ),
+atlantaToNewOrleans = ( $25 oneWay || $40 full-round ),
+atlantaToFlorida = ( $25 oneWay || $40 full-round );   ]}
+
+
+florida  ={
+ticketPrice =[
+floridaToLondon = ( $25 oneWay || $40 full-round ),
+floridaToCalifornia = ( $25 oneWay || $40 full-round ),
+floridaToHawaii = ( $25 oneWay || $40 full-round ),
+floridaToNewYork = ( $25 oneWay || $40 full-round ),
+floridaToDallas = ( $25 oneWay || $40 full-round ),
+floridaToNewOrleans = ( $25 oneWay || $40 full-round ),
+floridaToAtlanta = ( $25 oneWay || $40 full-round );   ]}
+
+
+
+
+}
+
+
+}
