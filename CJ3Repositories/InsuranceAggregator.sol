9b25870d9fcd7fa5dd772f820cc60cc73b1d8a74
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 13:36:12 -0600

    Update InsuranceAggregator.sol

--- CJ3Repositories/InsuranceAggregator.sol
+++ CJ3Repositories/InsuranceAggregator.sol
@@ -54,3 +54,10 @@ then return function sell, amountOf Pi;
 await function sell, amountOf Pi => amountOf USD;
 then return amountOf USD + insuranceProvider[balance] = insuranceProvider[newBalance]; }
 
+_____________________
+
+contingency: 80% revenue allocated to $CJ3Reserve as $USDC, 20% to sdk/api user;
+subscription fee: $25/ a month OR +5% revenue allocated to the owner address;
+amount $CJ3 to stake to deploy to mainnet: $100/ per contract using in sdk/api (allocate to $CJ3Reserves as $XLM)
+
+
