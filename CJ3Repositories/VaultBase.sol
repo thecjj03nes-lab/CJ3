efe244bc3b205f2f6d83bb2dd518606c02cb398e
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 13:36:43 -0600

    Update VaultBase.sol

--- CJ3Repositories/VaultBase.sol
+++ CJ3Repositories/VaultBase.sol
@@ -12,3 +12,8 @@ abstract contract VaultBase {
         if (sum != BPS) revert Errors.AllocationMismatch();
     }
 }
+_____________________
+
+contingency: 30% revenue allocated to $CJ3Reserve as $USDC, 70% to sdk/api user;
+subscription fee: $25/ a month OR +5% revenue allocated to the owner address;
+amount $CJ3 to stake to deploy to mainnet: $100/ per contract using in sdk/api (allocate to $CJ3Reserves as $XLM)
