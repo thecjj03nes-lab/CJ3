7f7c66629a678eb213154f4c79d40dd60ea755c8
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 13:15:24 -0600

    Create GoateDAO.sol

---
+++ CJ3Repositories/GoateDAO.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "../utils/Errors.sol";
+
+contract GoateDAO {
+    struct Proposal {
+        string description;
+        uint256 yes;
+        uint256 no;
+        uint256 deadline;
+        bool executed;
+    }
+
+    Proposal[] public proposals;
+
+    function propose(string calldata desc) external {
+        proposals.push(
+            Proposal(desc, 0, 0, block.timestamp + 75 days, false)
+        );
+    }
+
+    function vote(uint256 id, bool support) external payable {
+        Proposal storage p = proposals[id];
+        if (block.timestamp > p.deadline) revert Errors.InvalidVote();
+        if (support) p.yes += msg.value;
+        else p.no += msg.value;
+    }
+}
