2087161bd43d037d31d65a054910121315c346c3
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 13:27:04 -0600

    Update devHelper.sol

--- devHelper.sol
+++ devHelper.sol
@@ -3,3 +3,15 @@
 - choose their type of business / business they are creating (bank, exchange, utility company, telecommunications, government, prediction market, social media, other)
 - choose the different types of products they want implemented in their custom sdk/api from $CJ3 technology & repositories
 - devHelper helps them build their feature, webisite, app, and etc based on subscription + sdk/api contingency)
+
+
+
+1) create sdks/api for single contract functionality
+2) create sdks/api for multi contract functionality
+3) create apps, websites, dapps, contracts, and etc using sdk/apis
+4) deploy apps, websites, dapps, contracts, and etc to the testnet using sdk/apis
+5) deploy apps, websites, dapps, contracts, and etc to the mainnet using sdk/apis
+
+
+allow people to upload codes, urls, files, pictures, gifs, and etcetera to enhance their code; 
+each contact(s) sdk/api should have an allocated contingency based on usage, a subscription fee, and a stake to deploy to mainnet fee
