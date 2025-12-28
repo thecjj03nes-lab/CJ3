12d2d54076dc4355854514677573ce87fe387d8c
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 13:13:25 -0600

    Update GoateToken.sol

--- CJ3Repositories/GoateToken.sol
+++ CJ3Repositories/GoateToken.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GoateToken is ERC20, Ownable {
+    constructor() ERC20("Goate Electric", "GE") {
+        _mint(msg.sender, 1_000_000_000 ether);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
 
