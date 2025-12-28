7974a0a73d0b20c079559128ca1d38e05b4e9c43
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 12:49:39 -0600

    Update DualStorage.sol

--- CJ3Repositories/DualStorage.sol
+++ CJ3Repositories/DualStorage.sol
@@ -14,3 +14,38 @@ and the same with $MSTR:
 
 ($MSTR, $yBTC, $USDDenomination),
 ($MSTR, $WMT, $USDDenomination),
+
+A) https://www.dividend.com/how-to-invest/earn-dividend-income-every-week-12-stock-portfolio/
+
+B) ETFs
+
+C) Aquarius Protocol Yield Tokens
+________
+
+Use Dual-Storage with yield-bearing assets to upgrade $BTC DEFI to be insolvent; 
+
+Farm: yield allocates to owner & msg.sender
+
+Stake: yield allocates to owner & msg.sender stake balance
+
+Lock: yield allocates to owner & msg.sender
+
+Lend - yield allocates to owner, lender, and an availableBorrowPool
+
+Borrow - yield allocates to owner, loan balance, and availableBorrowPool
+_______
+
+2. (Assets, Chain, Digital Environment) Interoperability
+
+Move data & uint256 instantly; including texts, digital assets, stocks, and etc:
+
+- c2p
+- p2c
+- p2p
+- p2b
+- b2p
+- b2b
+
+https://github.com/cj03nes/GoateElectric/blob/main/contracts%20/pay/InstilledInteroperability.sol
+
+Allow $BTC to add outside tokens other than $USD to it's marketcap; this will help with native dual-storage too
