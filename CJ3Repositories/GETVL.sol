88c3ea20dea7d265cab0c8293fac4ded70437040
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 13:33:56 -0600

    Update GETVL.sol

--- CJ3Repositories/GETVL.sol
+++ CJ3Repositories/GETVL.sol
@@ -16,3 +16,10 @@ contract GETVL is VaultBase {
         asset.transferFrom(msg.sender, address(this), amount);
     }
 }
+
+
+_____________________
+
+contingency: 50% revenue allocated to $CJ3Reserve as $USDC, 50% to sdk/api user;
+subscription fee: $25/ a month OR +5% revenue allocated to the owner address;
+amount $CJ3 to stake to deploy to mainnet: $100/ per contract using in sdk/api (allocate to $CJ3Reserves as $XLM)
