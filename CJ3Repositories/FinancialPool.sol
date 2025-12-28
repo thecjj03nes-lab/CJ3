d7169abf9159d8345f3ca2e620f2ad811dd7d2f8
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 13:33:36 -0600

    Update FinancialPool.sol

--- CJ3Repositories/FinancialPool.sol
+++ CJ3Repositories/FinancialPool.sol
@@ -19,6 +19,7 @@ contract FinancialPool is VaultBase {
 
 _____________________
 
-contingency: 30% revenue allocated to $CJ3Reserve as $USDC, 70% to sdk/api user;
+contingency: 50% revenue allocated to $CJ3Reserve as $USDC, 50% to sdk/api user;
 subscription fee: $25/ a month OR +5% revenue allocated to the owner address;
 amount $CJ3 to stake to deploy to mainnet: $100/ per contract using in sdk/api (allocate to $CJ3Reserves as $XLM)
+
