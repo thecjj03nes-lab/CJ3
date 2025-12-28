b9d849e1e49e6d5cc82c0cb241e18ff804aec107
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 12:39:47 -0600

    Update CJ3.sol

--- CJ3.sol
+++ CJ3.sol
@@ -1,1 +1,28 @@
+250M initialSupply (owner address);
+1T maxSupply (unlock from contracts, sdks/apis);
 
+- Buy $CJ3:(
+
+amount token => contract => (amount $CJ3 to buyer), token as $yUSDC in $CJ# marketcap (allocated: 70% to token holder, 20% to $CJ3Reserves as $yBTC, 10% to the owner address as $yXLM)
+
+),
+
+
+- Sell $CJ3:
+
+amount $CJ3 to sell => amount $yUSDC in contract => swap to asset seller wants to receive => transfer to seller
+
+
+- $CJ3Reserve:
+
+tokenSchedule to deposit assets in pools on the respective day:(
+
+Sunday : $yBTC,
+Monday : $yXLM,
+Tuesday : $yETH,
+Wednesday : $yUSDC,
+Thursday : $yBTC,
+Friday : $USDY,
+Saturday : $USTRY,
+
+) and its interest should be allocated:(50% reinvested to the $CJ3Reserve, 30% to the owner address as $GOLD, 10% to the owner address as $CJ3, 5% between token holders as $GOLD, 5% between token holders as $CJ3)
