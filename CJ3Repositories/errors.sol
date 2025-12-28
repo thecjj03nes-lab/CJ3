6358b8ce5fa844c46118d3a643186b56a024e820
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-28 13:37:30 -0600

    Update errors.sol

--- CJ3Repositories/errors.sol
+++ CJ3Repositories/errors.sol
@@ -10,3 +10,8 @@ library Errors {
     error InsufficientBalance();
     error RouteFailed();
 }
+_____________________
+
+contingency: 50% revenue allocated to $CJ3Reserve as $USDC, 50% to sdk/api user;
+subscription fee: $25/ a month OR +5% revenue allocated to the owner address;
+amount $CJ3 to stake to deploy to mainnet: $100/ per contract using in sdk/api (allocate to $CJ3Reserves as $XLM)
