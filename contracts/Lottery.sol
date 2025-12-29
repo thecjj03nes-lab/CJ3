677a6a1ac23ebf29e4d552f66e7bf080d6fb6136
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-29 02:26:24 -0600

    Create Lottery.sol

---
+++ contracts/Lottery.sol
@@ -0,0 +1,14 @@
+a lottery opens for a set amount if time,
+
+
+
+people deposit money & it gets held as yUSDC, yBTC and etc Assets in the marketcap;
+
+
+the timer ends: async randomLotteryPick(random + random + random + random + random ÷ random + random + random + random + random + random + random + random ÷ random × random + random ÷ random = random;
+
+submit results;
+only LotteryPick with true results, timestamps, and allocation as bool verification (true/false) to async send them their funds;
+
+setLotteries on timer:
+allocatePotInterest: 20% $GETVL,
