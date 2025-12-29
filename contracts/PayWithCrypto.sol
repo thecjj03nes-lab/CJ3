ae0ec6d925f78624d86bf9119c2c2375847d5a0f
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-29 01:50:09 -0600

    Create PayWithCrypto.sol

---
+++ contracts/PayWithCrypto.sol
@@ -0,0 +1,18 @@
+token => $USDC => vendor
+token => $XLM=> vendor
+token => $PYUSD => vendor
+token => $USDT => vendor
+token => $BTC => vendor
+token => $CJ3 => vendor
+token => $GE => vendor
+token => $AQUA => vendor
+token => $GySt => vendor
+
+
+
+moonpay,
+stripe,
+forex,
+bankconnect,
+import virtual card,
+virtual card,
