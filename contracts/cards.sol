536a4fcad673e9f0f531e34bf483053e621f23b2
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-29 02:03:11 -0600

    Create cards.sol

---
+++ contracts/cards.sol
@@ -0,0 +1,18 @@
+spades.sol, 2v2 
+poker.sol, 1v1v1v1
+blackjack.sol, 1vcontract, 2vcontract, 3vcontract, 4vcontract
+uno.sol, 1v1v1v1 
+
+
+
+
+player joins session; (setban amount to join or different wage ranks)
+find session or create session; (wait for the right amount of players)
+
+
+
+
+
+
+
+p
