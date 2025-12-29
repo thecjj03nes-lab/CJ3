23f6bd52c7c1ce374ee51fb8eeb48ad9a8fc35db
thecjj03nes-lab <thecjj03nes@gmail.com>
2025-12-29 02:29:59 -0600

    Create TicTacToe.sol

---
+++ contracts/TicTacToe.sol
@@ -0,0 +1,18 @@
+join by depositingbmoney to a pot;
+
+1v1;
+
+find session or join session;
+
+a 3 by 3 box;
+
+
+player x;
+player o;
+
+the first time get 3 in a row, wins;
+horizontally,
+vertically,
+diagonally,
+
+winner gets the pot;
